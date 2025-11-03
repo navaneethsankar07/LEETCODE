c39b6a84e4f2d5d2e1f27f9abb43278a090f1242
Navaneeth Sankar <navaneethsankar07@gmail.com>
2025-11-03 09:02:12 +0530

    Time: 11 ms (54.95%), Space: 17.9 MB (49.89%) - LeetHub

--- 3184-count-pairs-that-form-a-complete-day-i/3184-count-pairs-that-form-a-complete-day-i.py
+++ 3184-count-pairs-that-form-a-complete-day-i/3184-count-pairs-that-form-a-complete-day-i.py
@@ -2,7 +2,7 @@ class Solution:
     def countCompleteDayPairs(self, hours: List[int]) -> int:
         length = len(hours)
         count = 0
-        for x in range(length):
+        for x in range(length-1):
             for y in range(x+1,length):
                 if (hours[x] + hours[y]) % 24 == 0:
                     count +=1
