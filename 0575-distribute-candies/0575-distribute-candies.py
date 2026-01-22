d3a7e920b1126e54c1cc4afe70c70d4c5f63199c
Navaneeth Sankar <navaneethsankar07@gmail.com>
2026-01-22 07:00:18 +0530

    Time: 24 ms (41.7%), Space: 21.1 MB (19.67%) - LeetHub

--- 0575-distribute-candies/0575-distribute-candies.py
+++ 0575-distribute-candies/0575-distribute-candies.py
@@ -1,4 +1,10 @@
 class Solution:
     def distributeCandies(self, candyType: List[int]) -> int:
-        unique = set(candyType)
-        return min(len(candyType)//2,len(unique))
+        len_can = len(candyType)//2
+        len_uni = len(set(candyType))
+        if len_can == len_uni:
+            return len_can
+        elif len_can > len_uni:
+            return len_uni
+        else:
+            return len_can
