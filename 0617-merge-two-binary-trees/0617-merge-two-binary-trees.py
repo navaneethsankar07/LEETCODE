6dc3450f82fa2a17584561c8379577618086922d
Navaneeth Sankar <navaneethsankar07@gmail.com>
2026-04-09 06:59:26 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0617-merge-two-binary-trees/0617-merge-two-binary-trees.py
+++ 0617-merge-two-binary-trees/0617-merge-two-binary-trees.py
@@ -6,9 +6,7 @@ class Solution:
         
         if not root2:
             return root1
-        
-        root1.val =  root1.val + root2.val
+        root1.val += root2.val
         root1.left = self.mergeTrees(root1.left, root2.left)
         root1.right = self.mergeTrees(root1.right, root2.right)
-
         return root1
