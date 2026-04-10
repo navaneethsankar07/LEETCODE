18a123c8188de5459f33505970af2eda5082f0f2
Navaneeth Sankar <navaneethsankar07@gmail.com>
2026-04-10 11:40:28 +0530

    Time: 57 ms (53.3%), Space: 25.7 MB (28.47%) - LeetHub

---
+++ 1268-search-suggestions-system/1268-search-suggestions-system.py
@@ -0,0 +1,46 @@
+class TrieNode:
+    def __init__(self):
+        self.children = {}
+        self.suggestions = []
+
+class Solution:
+    def __init__(self):
+        self.root = TrieNode()
+    
+    def suggestedProducts(self, products: List[str], searchWord: str) -> List[List[str]]:
+        products.sort()
+        
+        self.root = TrieNode()
+        
+        for product in products:
+            self.insert(product)
+        
+        return self.getSuggestions(searchWord)
+    
+    
+    def insert(self, word):
+        node = self.root
+        
+        for ch in word:
+            if ch not in node.children:
+                node.children[ch] = TrieNode()
+            
+            node = node.children[ch]
+            
+            if len(node.suggestions) < 3:
+                node.suggestions.append(word)
+    
+    
+    def getSuggestions(self, searchWord):
+        node = self.root
+        result = []
+        
+        for ch in searchWord:
+            if node and ch in node.children:
+                node = node.children[ch]
+                result.append(node.suggestions)
+            else:
+                node = None
+                result.append([])
+        
+        return result
