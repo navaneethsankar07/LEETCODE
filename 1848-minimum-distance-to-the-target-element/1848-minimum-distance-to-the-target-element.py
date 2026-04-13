b58804604cf6ef4d3a27695a649bd2d856be8380
Navaneeth Sankar <navaneethsankar07@gmail.com>
2026-04-13 06:53:15 +0530

    Time: 3 ms (18.99%), Space: 19.5 MB (24.63%) - LeetHub

---
+++ 1848-minimum-distance-to-the-target-element/1848-minimum-distance-to-the-target-element.py
@@ -0,0 +1,9 @@
+class Solution:
+    def getMinDistance(self, nums: List[int], target: int, start: int) -> int:
+        res = len(nums)
+
+        for i in range(len(nums)):
+            if nums[i] == target:
+                res = min(res, abs(i - start))
+        
+        return res
