aabe961e2211a33043b759577a8ac8b42f2474ff
Navaneeth Sankar <navaneethsankar07@gmail.com>
2026-06-06 06:24:25 +0530

    Time: 3 ms (74.11%), Space: 19.4 MB (86.97%) - LeetHub

--- 2574-left-and-right-sum-differences/2574-left-and-right-sum-differences.py
+++ 2574-left-and-right-sum-differences/2574-left-and-right-sum-differences.py
@@ -1,10 +1,14 @@
 class Solution:
     def leftRightDifference(self, nums: List[int]) -> List[int]:
-        left = []
-        right = []
-        for i in range(len(nums)):
-            left.append(sum(nums[:i]))
-            right.append(sum(nums[1+i:]))
-        for i in range(len(nums)):
-            nums[i] = abs(left[i] - right[i])
-        return nums
+        right_sum = sum(nums)
+
+        left_sum = 0
+        result = []
+
+        for num in nums:
+            right_sum -= num
+
+            result.append(abs(left_sum-right_sum))
+            left_sum += num
+        
+        return result
