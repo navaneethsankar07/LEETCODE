92ff22f77c266d9e788fe861c2fd67162c08e480
Navaneeth Sankar <navaneethsankar07@gmail.com>
2026-06-09 06:50:18 +0530

    Time: 21 ms (14.2%), Space: 19.1 MB (90.69%) - LeetHub

--- 1154-day-of-the-year/1154-day-of-the-year.py
+++ 1154-day-of-the-year/1154-day-of-the-year.py
@@ -1,5 +1,11 @@
-from datetime import datetime
 class Solution:
     def dayOfYear(self, date: str) -> int:
-        date = datetime.strptime(date, "%Y-%m-%d")
-        return date.timetuple().tm_yday
+        year, month, day = map(int, date.split('-'))
+
+        days = [31, 28, 31, 30, 31, 30,
+                31, 31, 30, 31, 30, 31]
+        
+        if (year % 400 == 0) or (year % 4 == 0 and year % 100 != 0):
+            days[1] = 29
+        
+        return sum(days[:month-1]) + day
