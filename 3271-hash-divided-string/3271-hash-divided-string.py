314c74a63d79a0bf5bdc6d30a3e90d161ec338f0
Navaneeth Sankar <navaneethsankar07@gmail.com>
2026-08-03 07:06:55 +0530

    Time: 19 ms (41.85%), Space: 19.2 MB (69.63%) - LeetHub

--- 3271-hash-divided-string/3271-hash-divided-string.py
+++ 3271-hash-divided-string/3271-hash-divided-string.py
@@ -1,13 +1,13 @@
 class Solution:
     def stringHash(self, s: str, k: int) -> str:
-        result = ''
+        result = []
         total = 0
 
         for i, char in enumerate(s):
             total += ord(char) - 97
 
             if (i+1) % k == 0:
-                result += chr((total%26) + 97)
+                result.append(chr((total%26) + 97))
                 total = 0
         
-        return result 
+        return ''.join(result)
