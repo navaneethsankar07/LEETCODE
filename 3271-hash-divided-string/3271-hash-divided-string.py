382ac46106dc379a4fbd73fcd461a9f1e4d708d9
Navaneeth Sankar <navaneethsankar07@gmail.com>
2026-08-03 06:59:53 +0530

    Time: 11 ms (95.19%), Space: 19.3 MB (69.63%) - LeetHub

--- 3271-hash-divided-string/3271-hash-divided-string.py
+++ 3271-hash-divided-string/3271-hash-divided-string.py
@@ -1,10 +1,9 @@
 class Solution:
     def stringHash(self, s: str, k: int) -> str:
-        substrings = [s[x:x+k] for x in range(0,len(s),k)]
         result = ''
-        for sub in substrings:
+        for x in range(0, len(s), k):
             total = 0
-            for x in sub:
+            for x in s[x:x+k]:
                 total += ord(x) - 97
             
             total = total % 26
