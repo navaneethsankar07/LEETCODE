ddfbfa82d0e492e7b945e79dcb62cf520c29af34
Navaneeth Sankar <navaneethsankar07@gmail.com>
2026-08-17 08:12:22 +0530

    Time: 53 ms (86.6%), Space: 19.3 MB (17.45%) - LeetHub

--- 3079-find-the-sum-of-encrypted-integers/3079-find-the-sum-of-encrypted-integers.py
+++ 3079-find-the-sum-of-encrypted-integers/3079-find-the-sum-of-encrypted-integers.py
@@ -2,12 +2,18 @@ class Solution:
     def sumOfEncryptedInt(self, nums: List[int]) -> int:
         total = 0
         for x in nums:
-            if len(str(x)) > 1:
-                max_digit = max(str(x))
-                new_num = max_digit * len(str(x))
-                total += int(new_num)
+            max_digit = 0
+            ones_base = 0
+
+            while x > 0:
+                digit = x % 10
+                if digit > max_digit:
+                    max_digit = digit
+
+                ones_base = ones_base * 10 + 1
+
+                x //= 10
             
-            else:
-                total += x
+            total +=  max_digit * ones_base
         
         return total
