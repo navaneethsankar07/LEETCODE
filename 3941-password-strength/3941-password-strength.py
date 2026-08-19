10c49d6144bacf6da210ae0749cedd5559bc4e9f
Navaneeth Sankar <navaneethsankar07@gmail.com>
2026-08-19 07:26:36 +0530

    Time: 3 ms (98.27%), Space: 19.7 MB (89.15%) - LeetHub

--- 3941-password-strength/3941-password-strength.py
+++ 3941-password-strength/3941-password-strength.py
@@ -1,18 +1,18 @@
 class Solution:
     def passwordStrength(self, password: str) -> int:
-        strength = 0
+        a=list(set(password))
         alpha="qwertyuiopasdfghjklzxcvbnm"
         ALPHA="QWERTYUIOPASDFGHJKLZXCVBNM"
         num="1234567890"
-        for x in set(password):
+        result=0
+        for x in a:
             if x in alpha:
-                strength+=1
+                result+=1
             elif x in ALPHA:
-                strength+=2
+                result+=2
             elif x in num:
-                strength+=3
+                result+=3
             else:
-                strength+=5
-                    
-        return strength
-            
+                result+=5
+        return result
+        
