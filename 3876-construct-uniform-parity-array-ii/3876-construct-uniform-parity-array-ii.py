6fb8ce38f2ef804d1c55647cf5118b8c7f7c0bfa
Navaneeth Sankar <navaneethsankar07@gmail.com>
2026-09-03 08:01:19 +0530

    Time: 16 ms (84.76%), Space: 35.3 MB (51.83%) - LeetHub

--- 3876-construct-uniform-parity-array-ii/3876-construct-uniform-parity-array-ii.py
+++ 3876-construct-uniform-parity-array-ii/3876-construct-uniform-parity-array-ii.py
@@ -1,11 +1,3 @@
 class Solution:
-    def uniformArray(self, nums1: list[int]) -> bool:
-        _min = float('inf')
-        odd_count = 0
-
-        for x in nums1:
-            _min = min(_min, x)
-            if x % 2 == 1:
-                odd_count += 1
-        
-        return _min % 2 == 1 or odd_count == 0
+    def uniformArray(self, A: list[int]) -> bool:
+        return min(A) % 2 == 1 or all(a % 2 == 0 for a in A)
